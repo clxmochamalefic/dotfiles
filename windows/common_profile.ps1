6af11789ac7c8aa197607c469101eed7a211526f
cocoalix <cocoalix@gmail.com>
2023-02-24 15:16:06 +0900

    add init files

--- windows/common_profile.ps1
+++ windows/common_profile.ps1
@@ -3,4 +3,5 @@ function ssho() { ssh -o ServerAliveInterval=60 $args }
 function which() { Get-Command $args | Format-List }
 function sh() { pwsh $args }
 
+Import-Module posh-git
 oh-my-posh init pwsh --config $env:POSH_THEMES_PATH/paradox.omp.json | Invoke-Expression
