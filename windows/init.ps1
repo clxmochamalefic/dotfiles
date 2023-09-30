0189cea75f05ef3c03e244b000cd33562fd7b4a8
cocoalix <cocoalix@gmail.com>
2023-09-30 18:05:42 +0900

    update preferences

--- windows/init.ps1
+++ windows/init.ps1
@@ -1,9 +1,21 @@
+#!/usr/bin/env pwsh
+
+# copy pwsh preference
 $profilePath = "$Home\Documents\PowerShell\"
 if ([System.IO.Directory]::Exists($profilePath) -eq $false) {
   New-Item -Path $profilePath -ItemType Directory
 }
 Get-Content "${PSScriptRoot}\preferences\common_profile.ps1" > "${profilePath}Microsoft.PowerShell_profile.ps1"
 
+# ~/.wslconfig
+$wslconfigPath = "$Home\.wslconfig"
+if ([System.IO.Directory]::Exists($wslconfigPath) -eq $false) {
+  New-Item -Path $profilePath -ItemType Directory
+}
+Get-Content "${PSScriptRoot}\preferences\common_profile.ps1" > "${profilePath}Microsoft.PowerShell_profile.ps1"
+
+# install posh-git (show git branch on prompt)
 Install-Module posh-git
 
+# reload profile
 . $PROFILE
---
+++ windows/preferences/README.md
@@ -0,0 +1,14 @@
+# dotfiles/windows/preferences
+
+## .wslconfig
+
+wsl2 global instance setting
+
+### [REFERENCE] wsl configure
+
+> see: https://learn.microsoft.com/ja-jp/windows/wsl/wsl-config
+
+## common_profile.ps1
+
+default pwsh profile for me
+
