d02116daa865c2c785726162fc37cd25b913376c
cocoalix <cocoalix@gmail.com>
2024-08-02 11:30:15 +0900

    fix: add command `denopsup` and `reload` to command alias

--- windows/pwsh/preference/command_aliases.ps1
+++ windows/pwsh/preference/command_aliases.ps1
@@ -1,18 +1,54 @@
+
 function nq()
 {
   nvim-qt ~ 
 }
+
+# gui neovim
+function gn()
+{
+  # TODO: gcmでgui-nvimを探す処理が別途必要のはず
+  neovide ~ 
+}
+
 function ssho()
 {
   ssh -o ServerAliveInterval=60 $args 
 }
+
 function which()
 {
   Get-Command $args | Format-List 
 }
+
+function where()
+{
+  gcm $args
+}
+
 function sh()
 {
   pwsh $args 
 }
 
+# denops server up
+function denopsup()
+{
+  $startupPath = [Environment]::GetEnvironmentVariable('Startup', 'USER')
+  $denopsVbsPath = "${startupPath}\boot_denops.vbs"
+  Start-Process $denopsVbsPath
+}
+
+# denops server up
+function deup()
+{
+  denopsup
+}
+
+
+function reload()
+{
+  Write-Output 'RELOAD profile'
+    . $PROFILE
+}
 
