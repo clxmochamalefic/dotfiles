3ca12553b21f8b9021d07a1ca0695b72252d80ed
clxmochamalefic <5728720+clxmochamalefic@users.noreply.github.com>
2024-11-19 11:29:41 +0900

    fix: rename other-nvim filename

--- nvim/lua/plugins/plugin/ui/fzf/init.lua
+++ nvim/lua/plugins/plugin/ui/fzf/init.lua
@@ -2,22 +2,17 @@
 -- TEELSCOPE PLUGINS
 -- ---------------------------------------------------------------------------
 
+local tls = require("plugins.plugin.ui.fzf.telescope")
+--local oth = require("plugins.plugin.ui.fzf.other")
+local ext = require("plugins.plugin.ui.fzf.ext")
+
 local M = {}
 
-local tls = require("plugins.plugin.ui.fzf.telescope")
-
-local ext_fzf = require("plugins.plugin.ui.fzf.ext.fzf-native")
-local ext_lga = require("plugins.plugin.ui.fzf.ext.live-grep-args")
-local ext_mem = require("plugins.plugin.ui.fzf.ext.memo")
-
-local oth = require("plugins.plugin.ui.fzf.other-nvim")
-
 table.insert(M, tls)
+--table.insert(M, oth)
 
-table.insert(M, ext_fzf)
-table.insert(M, ext_lga)
-table.insert(M, ext_mem)
-
-table.insert(M, oth)
+for _, v in ipairs(ext) do
+  table.insert(M, v)
+end
 
 return M
---
+++ nvim/lua/plugins/plugin/ui/fzf/other.lua
@@ -0,0 +1,59 @@
+return {
+  {
+    lazy = true,
+    "rgroli/other.nvim",
+    cmd = { "Other" },
+    keys = {
+      { "<leader>.", "<cmd>Other<CR>", { mode = "n", silent = true, desc = "Telescope: buffers" } },
+    },
+    opts = {
+      mappings = {
+        -- builtin mappings
+        "livewire",
+        "angular",
+        "laravel",
+        "rails",
+        "golang",
+        -- custom mapping
+        {
+          pattern = "/path/to/file/src/app/(.*)/.*.ext$",
+          target = "/path/to/file/src/view/%1/",
+          transformer = "lowercase",
+        },
+      },
+      keybindings = {
+        ["<cr>"] = "open_file_by_command()",
+        ["<esc>"] = "close_window()",
+        q = "close_window()",
+        t = "open_file_tabnew()",
+        o = "open_file()",
+        v = "open_file_vs()",
+        s = "open_file_sp()",
+      },
+      transformers = {
+        -- defining a custom transformer
+        lowercase = function(inputString)
+          return inputString:lower()
+        end,
+      },
+      style = {
+        -- How the plugin paints its window borders
+        -- Allowed values are none, single, double, rounded, solid and shadow
+        border = "solid",
+
+        -- Column seperator for the window
+        seperator = "|",
+
+        -- width of the window in percent. e.g. 0.5 is 50%, 1.0 is 100%
+        width = 0.7,
+
+        -- min height in rows.
+        -- when more columns are needed this value is extended automatically
+        minHeight = 2,
+      },
+    },
+    config = function(_, opts)
+      require("other-nvim").setup(opts)
+    end,
+  },
+}
