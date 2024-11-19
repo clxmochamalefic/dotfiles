a8d92e772e8500bf81a37b693d2243bf023ffe4e
clxmochamalefic <5728720+clxmochamalefic@users.noreply.github.com>
2024-11-19 11:33:35 +0900

    fix: lazy position modify

--- nvim/lua/plugins/plugin/ui/fzf/ext/fzf-native.lua
+++ nvim/lua/plugins/plugin/ui/fzf/ext/fzf-native.lua
@@ -11,11 +11,11 @@ end
 
 return {
   {
-    lazy = true,
     "nvim-telescope/telescope-fzf-native.nvim",
     dependencies = {
       "nvim-telescope/telescope.nvim",
     },
+    lazy = true,
     build =
     "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
     config = function()
