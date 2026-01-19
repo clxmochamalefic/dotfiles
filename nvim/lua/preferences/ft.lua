9e8101efc0c30e21d3cabfce89386bafdde90398
clxmochamal <5728720+clxmochamalefic@users.noreply.github.com>
2026-01-19 09:34:13 +0900

    fix: separate `setup()` function in `ft.lua`

--- nvim/lua/preferences/ft.lua
+++ nvim/lua/preferences/ft.lua
@@ -1,14 +1,15 @@
 local ft = {}
 
-local opt = vim.opt
-local fn = vim.fn
-local api = vim.api
-local g = vim.g
-
 ft.setup = function()
+  -- TODO: おそらく `ft` に対するものと `ft` を補正するものに分けたほうがいい
+  ft.tabstop()
+  ft.filetype()
+end
+
+ft.tabstop = function()
   -- define tabstop by filetype
-  local augroup = api.nvim_create_augroup('FileTypeIndent', { clear = true })
-  api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+  local augroup = vim.api.nvim_create_augroup('FileTypeIndent', { clear = true })
+  vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
     group = augroup,
     pattern = {
       "*.html",
@@ -31,17 +32,31 @@ ft.setup = function()
     },
     callback = function ()
       -- do something
-      opt.tabstop = 2
+      vim.opt.tabstop = 2
     end
   })
-  api.nvim_create_autocmd("FileType", {
+  vim.api.nvim_create_autocmd("FileType", {
     group = augroup,
     pattern = {
       "blade",
     },
     callback = function ()
       -- do something
-      opt.tabstop = 2
+      vim.opt.tabstop = 2
+    end
+  })
+end
+
+ft.filetype = function()
+  local augroup = vim.api.nvim_create_augroup('CmakePreference', { clear = true })
+  vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+    group = augroup,
+    pattern = {
+      "*CMakeLists.txt",
+    },
+    callback = function ()
+      -- do something
+      vim.opt.ft = "cmake"
     end
   })
 end
