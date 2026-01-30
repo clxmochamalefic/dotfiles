709946d329a36a82aada411917e2418d45e557d8
clxmochamal <5728720+clxmochamalefic@users.noreply.github.com>
2026-01-30 09:55:51 +0900

    feat: add `%WINTERM_PATH%` user env var and use what on WindowsTerminal preference

---
+++ term/windowsterminal/README.md
@@ -0,0 +1,9 @@
+# terminal preference for `WindowsTerminal`
+
+## build.deno.ts
+
+make `WindowsTerminal` settings.json file from `src`
+
+## Update-WindowsTerminalLocation.ps1
+
+update `$env:WINTERM_PATH` user env variable from `WindowsTerminal` executable location for `WindowsTerminal` icon preference
---
+++ term/windowsterminal/Update-WindowsTermLocation.ps1
@@ -0,0 +1,7 @@
+$location = Get-AppxPackage | where { $_.Name -eq "Microsoft.WindowsTerminal" } | Select -ExpandProperty InstallLocation
+$env_name = "WINTERM_PATH"
+$env_type = "User"
+
+# 新しい環境変数を設定する
+echo "update ${env_type} ENV: Name:${env_name} / Val:" + $location
+[System.Environment]::SetEnvironmentVariable($env_name, $location, $env_type)
