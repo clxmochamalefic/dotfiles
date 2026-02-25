4aac4781856d606c0aff2a5a8340d05bd1f6d722
clxmochamal <5728720+clxmochamalefic@users.noreply.github.com>
2026-02-25 11:39:15 +0900

    feat: add `folke/trouble.nvim`

--- nvim/lua/plugins/rc/lsp/lsp.lua
+++ nvim/lua/plugins/rc/lsp/lsp.lua
@@ -313,4 +313,18 @@ return {
       require("actions-preview").setup({})
     end,
   },
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      { "<leader>x", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",           desc = "Buffer Diagnostics (Trouble)", },
+      { "<leader>X", "<cmd>Trouble diagnostics toggle<cr>",                        desc = "Diagnostics (Trouble)", },
+      --{ "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>",                desc = "Symbols (Trouble)", },
+      --{ "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions / references / ... (Trouble)", },
+      --{ "<leader>xL", "<cmd>Trouble loclist toggle<cr>",                            desc = "Location List (Trouble)", },
+      --{ "<leader>xQ", "<cmd>Trouble qflist toggle<cr>",                             desc = "Quickfix List (Trouble)", },
+    },
+  },
 }
+
