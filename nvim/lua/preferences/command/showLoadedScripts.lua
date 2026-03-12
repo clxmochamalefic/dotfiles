6d6c6a252bdf92d2eeb65cb24ccef4f01d744602
clxmochamal <5728720+clxmochamalefic@users.noreply.github.com>
2026-03-12 10:39:24 +0900

    feat: add debug command for neovim

--- nvim/lua/preferences/command/showLoadedScripts.lua
+++ nvim/lua/preferences/command/showLoadedScripts.lua
@@ -1,10 +1,15 @@
 local M = {}
 
 M.setup = function()
-  local function f()
+  local function ls()
     vim.cmd([[enew|pu=execute('scriptnames')]])
   end
-  vim.api.nvim_create_user_command("ShowLoadedScripts", f, {})
+  vim.api.nvim_create_user_command("ShowLoadedScripts", ls, {})
+
+  local function ac()
+    vim.cmd([[enew|pu=execute('autocmd')]])
+  end
+  vim.api.nvim_create_user_command("ShowAutoCommands", ac, {})
 end
 
 return M
