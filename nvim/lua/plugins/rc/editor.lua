655c829c79cf38f115cc8a8ec9c0681a586cbf42
clxmochamal <5728720+clxmochamalefic@users.noreply.github.com>
2026-04-01 09:28:02 +0900

    fix: enable `Comment.nvim`

--- nvim/lua/plugins/rc/editor.lua
+++ nvim/lua/plugins/rc/editor.lua
@@ -1,12 +1,24 @@
 
 return {
   {
+    lazy = true,
     'numToStr/Comment.nvim',
+    event = { 'FileReadPost', 'VeryLazy', },
     opts = {
         -- add any options here
+        ignore = '^$',
+        -- 単行
+        toggler = {
+          -- <C-/> は設定として機能しないため、<C-_> を使用
+          line = '<C-_>',
+          --block = '<C-_>',
+        },
+        -- 複数行
+        opleader = {
+          -- <C-/> は設定として機能しないため、<C-_> を使用
+          line = '<C-_>',
+          --block = '<leader>b',
+        },
     },
-    config = function()
-      require('Comment').setup()
-    end,
   }
 }
