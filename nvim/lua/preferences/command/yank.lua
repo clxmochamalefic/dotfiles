486609673b81c58b9b363771e19e8f2c7501e5b9
clxmochamal <5728720+clxmochamalefic@users.noreply.github.com>
2026-05-26 10:03:50 +0900

    fix: add command alias to `ClipFileName` and add command `ClipDirPath`

--- nvim/lua/preferences/command/yank.lua
+++ nvim/lua/preferences/command/yank.lua
@@ -12,9 +12,29 @@ local function clipFilePath()
   vim.notify('Copied file path: ' .. v, vim.log.levels.INFO)
 end
 
+local function clipDirPath()
+  local v = vim.fn.expand('%:p:h')
+  vim.cmd([[let @+ = expand('%:p:h')]])
+  vim.notify('Copied dir path: ' .. v, vim.log.levels.INFO)
+end
+
 M.setup = function()
-  vim.api.nvim_create_user_command("ClipFileName", clipFileName, {})
-  vim.api.nvim_create_user_command("ClipFilePath", clipFilePath, {})
+  local opts_clip_file_name = {
+    desc = "copy to OS clipboard: current buffer file name",
+  }
+  local opts_clip_file_path = {
+    desc = "copy to OS clipboard: current buffer file path",
+  }
+  local opts_clip_dir_path  = {
+    desc = "copy to OS clipboard: current buffer dir path",
+  }
+
+  vim.api.nvim_create_user_command("ClipFileName", clipFileName, opts_clip_file_name)
+  vim.api.nvim_create_user_command("ClipFilePath", clipFilePath, opts_clip_file_path)
+  vim.api.nvim_create_user_command("ClipDirPath",  clipDirPath,  opts_clip_dir_path)
+  vim.api.nvim_create_user_command("YankFileName", clipFileName, opts_clip_file_name)
+  vim.api.nvim_create_user_command("YankFilePath", clipFilePath, opts_clip_file_path)
+  vim.api.nvim_create_user_command("YankDirPath",  clipDirPath,  opts_clip_dir_path)
 end
 
 return M
